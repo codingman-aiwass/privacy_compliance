d959379dac03cdb8aea79e110019d256ad16892e
codingman-aiwass <abc1277046286@gmail.com>
2023-11-16 20:32:10 +0800

    修改了启动frida-server脚本的逻辑，删除了启动动态分析之前清空所有第三方app的逻辑

--- prepareInDocker_linux.sh
+++ prepareInDocker_linux.sh
@@ -4,10 +4,10 @@ adb devices
 frida_server_running=$(adb shell ps -e | grep frida15)
 
 if [[ -n $frida_server_running ]]; then
-  echo "frida-server is running. Killing..."
-  adb shell su -c killall frida15
-  echo "Starting..."
-  adb shell su -c ./data/local/tmp/frida15 &
+  echo "frida-server is running. No need to start..."
+#  adb shell su -c killall frida15
+#  echo "Starting..."
+#  adb shell su -c ./data/local/tmp/frida15 &
 else
   echo "frida-server is not running. Starting..."
   adb shell su -c ./data/local/tmp/frida15 &
